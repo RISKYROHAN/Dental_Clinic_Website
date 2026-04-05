c431f33e329ea0b03fc4355cc9b3800795e83846
RISKYROHAN <rahul199741@gmail.com>
2026-04-06 02:22:01 +0530

    incomplete_chatbot commit

--- test_ai.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-const ai = require('ai');
-const dataStreamKeys = Object.keys(ai).filter(key => key.toLowerCase().includes('data'));
-const resKeys = Object.keys(ai).filter(k => k.toLowerCase().includes('response'));
-fs.writeFileSync('output2.txt', JSON.stringify({ dataStreamKeys, resKeys }, null, 2), 'utf8');
